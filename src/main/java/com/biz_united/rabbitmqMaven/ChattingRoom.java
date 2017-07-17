3f2ccab840fa7a220fdf4f716a22d1225ff41057
King <328592742@qq.com>
2017-07-17 09:56:30 +0800

    一个聊天室

---
+++ src/main/java/com/biz_united/rabbitmqMaven/ChattingRoom.java
@@ -0,0 +1,209 @@
+package com.biz_united.rabbitmqMaven;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class ChattingRoom extends javax.swing.JFrame implements ActionListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	//
+	private JButton entryChattingRoom;
+	private JTextArea inputUserName;
+	private JLabel text;
+	private ChattingRoomSetup chattingRoomSetup;
+	private JTextArea inputTextArea;
+	private JButton sendText;
+	private int count = 1;
+	private JPanel messageContainer = null;
+
+	//
+	private JScrollPane messageWindow;
+	
+	public ChattingRoom() {
+		super();
+		this.setLayout(null);
+		this.setSize(300, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.getContentPane().setLayout(null);
+		this.add(getInputUserName(), null);
+		this.add(getText(), null);
+		this.add(getLoginButton(), null);
+		entryChattingRoom.addActionListener(loginAction);
+		this.setTitle("Login");
+		this.setVisible(true);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent we){
+				try {
+					//chattingRoomSetup.getChannel().queueDelete(chattingRoomSetup.getUserName()+"MessageQueue");
+					chattingRoomSetup.sendMessage(chattingRoomSetup.getChannel(), chattingRoomSetup.getUserName()+" has quit the chattingroom");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	private JButton getLoginButton() {
+		if (entryChattingRoom == null) {
+			entryChattingRoom = new JButton();
+			entryChattingRoom.setBounds(100, 110, 80, 25);
+			entryChattingRoom.setText("Login");
+		}
+		return entryChattingRoom;
+	}
+
+	private JTextArea getInputUserName() {
+		if (inputUserName == null) {
+			inputUserName = new JTextArea();
+			inputUserName.setBounds(130, 60, 110, 18);
+		}
+		return inputUserName;
+	}
+
+	private JLabel getText() {
+		if (text == null) {
+			text = new JLabel();
+			text.setBounds(45, 60, 80, 18);
+			text.setText("User Name:");
+		}
+		return text;
+	}
+
+	// JSscrollPane只能容纳一个组件
+	private JScrollPane getMessageWindow() {
+		if (messageWindow == null) {
+			messageWindow = new JScrollPane();
+			messageWindow.setBounds(5, 5, 578, 400);
+		}
+		return messageWindow;
+	}
+
+	private JTextArea getTextInputArea() {
+		if (inputTextArea == null) {
+			inputTextArea = new JTextArea();
+			inputTextArea.setBounds(5, 410, 500, 145);
+			inputTextArea.setFont(new Font("宋体", Font.BOLD, 16));
+		}
+		return inputTextArea;
+	}
+
+	private JButton getSendButton() {
+		if (sendText == null) {
+			sendText = new JButton();
+			sendText.setText("Send");
+			sendText.setBounds(510, 460, 70, 40);
+		}
+		return sendText;
+	}
+
+	private JPanel getMessageContainer() {
+		if (messageContainer == null) {
+			messageContainer = new JPanel();
+			messageContainer.setLayout(null);
+			messageContainer.setBackground(Color.WHITE);
+		}
+		return messageContainer;
+	}
+
+	private ActionListener loginAction = new ActionListener() {
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if (!inputUserName.getText().equals("")) {
+				setTitle(inputUserName.getText());
+				chattingRoomSetup = new ChattingRoomSetup(inputUserName.getText());
+				entryChattingRoom();
+				Timer timer = new Timer();
+				timer.schedule(new TimerTask() {
+					public void run() {
+						try {
+							chattingRoomSetup.receiveMessage(chattingRoomSetup);
+							if (!chattingRoomSetup.getMsg().equals("")) {
+								showMessage(chattingRoomSetup.getMsg());
+								chattingRoomSetup.setMsg("");
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}, 200, 200);
+			}
+		}
+	};
+
+	private ActionListener sendMessage = new ActionListener() {
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if (!inputTextArea.getText().equals("")) {
+				try {
+					chattingRoomSetup.sendMessage(chattingRoomSetup.getChannel(),
+							chattingRoomSetup.getUserName() + " say:" + inputTextArea.getText());
+					inputTextArea.setText(null);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		}
+	};
+
+	private void entryChattingRoom() {
+		JPanel jPanel = (JPanel) getContentPane();
+		setSize(600, 600);
+		jPanel.add(getMessageWindow(), null);
+		messageWindow.setVisible(true);
+		jPanel.remove(text);
+		jPanel.remove(inputUserName);
+		jPanel.remove(entryChattingRoom);
+		jPanel.add(getTextInputArea(), null);
+		jPanel.add(getSendButton(), null);
+		sendText.addActionListener(sendMessage);
+		repaint();
+	}
+
+	private void showMessage(String msg) {
+		System.out.println(msg);
+		JScrollPane panel = getMessageWindow();
+		JPanel messageContainer = getMessageContainer();
+		JLabel showMessage = new JLabel();
+		showMessage.setFont(new Font("宋体", Font.BOLD, 16));
+		showMessage.setBounds(10, count * 30, 500, 30);
+		showMessage.setText(msg);
+		showMessage.setVisible(true);
+		messageContainer.add(showMessage);
+		panel.setViewportView(messageContainer);
+		// panel.add(showMessage,null);
+		panel.repaint();
+		count++;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		new ChattingRoom();
+	}
+
+}
---
+++ src/main/java/com/biz_united/rabbitmqMaven/ChattingRoomSetup.java
@@ -0,0 +1,82 @@
+package com.biz_united.rabbitmqMaven;
+
+import java.io.IOException;
+import com.rabbitmq.client.*;
+
+public class ChattingRoomSetup {
+
+	private Connection connection = null;
+	private Channel channel = null;
+	private Consumer consumer = null;
+	private String userName = null;
+	private String msg = "";
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public Connection getConnection() {
+		return connection;
+	}
+
+	public Consumer getConsumer() {
+		return consumer;
+	}
+
+	public Channel getChannel() {
+		return channel;
+	}
+
+	public ChattingRoomSetup(String userName) {
+
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setHost("localhost");
+			factory.setPort(5672);
+			factory.setUsername("guest");
+			factory.setPassword("guest");
+			factory.setVirtualHost("/");
+			this.connection = factory.newConnection();
+			this.channel = connection.createChannel();
+			this.userName = userName;
+			
+			channel.exchangeDeclare("chattingRoomExchange", "fanout", false, true, null);
+			String queueSet = userName + "MessageQueue";
+			channel.queueDeclare(queueSet, false, false, true, null);
+			channel.queueBind(queueSet, "chattingRoomExchange", "");
+			String message = userName + " has joined the chattingroom";
+			channel.basicPublish("chattingRoomExchange", userName, null, message.getBytes());
+			consumer = new DefaultConsumer(channel) {
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+						byte[] body) throws IOException {
+					String message = new String(body, "UTF-8");
+					if (!message.equals("") || !message.equals(null)) {
+						msg = message;
+					}
+					// System.out.println(userName+" have received '" + message
+					// + "'");
+				}
+			};
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	public void sendMessage(Channel channel, String message) throws IOException {
+		channel.basicPublish("chattingRoomExchange", "", null, message.getBytes());
+	}
+
+	public void receiveMessage(ChattingRoomSetup chattingRoomSetup) throws IOException {
+		channel.basicConsume(chattingRoomSetup.getUserName() + "MessageQueue", true, chattingRoomSetup.getConsumer());
+	}
+}
